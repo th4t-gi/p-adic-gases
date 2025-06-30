704b0ab8fdc7e324e31b6aba08af96af9c5bbbbc
th4t-gi <juddbrau@gmail.com>
2025-06-30 09:22:44 -0500

    Added most recent scripts

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 from typing import List
 import numpy as np
+import sqlite3
+import pandas as pd
 
 def falling_factorial(x,n):
     prod = 1.0
@@ -13,22 +15,27 @@ def factor(branch: int, degree: int, p: int, e_J: int, beta: float):
     return falling_factorial(p, degree)/denom
 
 
-def weight(branches: List[int], degrees: List[int], p: int, interaction_enrgy: List[float], beta: float):
+def weight(branches: List[int], p: int, energies: List[float], beta: float):
     total = 0.0
-    for J,degree in zip(branches, degrees):
-        total += factor(J, degree, p, interaction_enrgy[J], beta)
+    for J in branches:
+        size_J = bin(J).count("1")
+        e_J = energies[J]
+        
+        factor = e_J/(1-p**(1-size_J - e_J*beta))
+        total += factor
     return total
 
-def term(branches: List[int], degrees: List[int], p: int, interaction_enrgy: List[float], beta: float):
+def term(branches: List[int], degrees: List[int], p: int, energies: List[float],  beta: float):
     out = 1.0
     for J,degree in zip(branches, degrees):
-        out *= factor(J, degree, p, interaction_enrgy[J], beta)
+        out *= factor(J, degree, p, energies[J], beta)
     return out
 
 
-def interaction_energy(charges, size_of_charge): 
-    out = np.empty(pow(2, size_of_charge), dtype= float)
-    for J in range (0,  pow(2, size_of_charge)):
+def interaction_energy(charges: List[int]): 
+    size_of_charges = len(charges)
+    out = np.empty(pow(2, size_of_charges), dtype= float)
+    for J in range (0, pow(2, size_of_charges)):
         sum1 = 0.0
         sum2 = 0.0
         for i,charge in enumerate(charges):
@@ -36,3 +43,48 @@ def interaction_energy(charges, size_of_charge):
             sum2 += ((bool(J & (1 << i))) * charge* charge)
         out[J] = (((sum1 * sum1) - sum2) / 2.0)
     return out
+
+
+def query(n: int, p: int) -> pd.DataFrame:
+    # create connection
+    dbname = f"./size{n}.db"
+    # print(dbname)
+    con = sqlite3.connect(dbname)
+    
+    # Read query results into a pandas DataFrame
+    query = f"""
+        SELECT
+            trees.id, trees.branches, trees.degrees, probabilities.prime, probabilities.probability
+        FROM
+            probabilities
+        JOIN
+            trees ON probabilities.tree_id = trees.id
+        WHERE
+            probabilities.prime = {p}
+        ORDER BY
+            probabilities.probability DESC
+    """
+    df = pd.read_sql_query(query, con)
+    con.close()
+    
+    return df
+
+# from https://stackoverflow.com/questions/893657/how-do-i-calculate-r-squared-using-python-and-numpy
+# Polynomial Regression
+def polyfit(x, y, degree):
+    results = {}
+
+    coeffs = np.polyfit(x, y, degree)
+
+     # Polynomial Coefficients
+    results['polynomial'] = coeffs
+    # r-squared
+    p = np.poly1d(coeffs)
+    # fit values, and mean
+    yhat = p(x)                         # or [p(z) for z in x]
+    ybar = np.sum(y)/len(y)          # or sum(y)/len(y)
+    ssreg = np.sum((yhat-ybar)**2)   # or sum([ (yihat - ybar)**2 for yihat in yhat])
+    sstot = np.sum((y - ybar)**2)    # or sum([ (yi - ybar)**2 for yi in y])
+    results['determination'] = ssreg / sstot
+
+    return results
