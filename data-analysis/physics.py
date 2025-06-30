60644163220fbfd448d170646d1ff67edfc5dc0f
th4t-gi <juddbrau@gmail.com>
2025-06-30 14:44:23 -0500

    refactored python scripts and put them in their own directory

--- data-analysis/physics.py
+++ data-analysis/physics.py
@@ -1,24 +1,23 @@
 from typing import List
 
-import sqlite3
 import pandas as pd
 import numpy as np
 import ast
 import math
-from utils import interaction_energy, term, weight
+from utils import interaction_energy, term, weight, query
 import matplotlib.pyplot as plt
 import matplotlib.widgets as mwidgets
 from matplotlib.animation import FuncAnimation
 
 # primes = [7] # primes to compute probabilities for
-primes = [7]
+primes = [2,3,5,7,11,13]
 # input("what prime do you want to see")
-beta_resolution = 100 # resolution (number of points, so 1 more than the number of gaps)
+beta_resolution = 1000 # resolution (number of points, so 1 more than the number of gaps)
 set_charges = [1,-1,1,-1]
 n = len(set_charges)
 
 # calculates interaction energies and sigmas 
-energies = interaction_energy(set_charges,len(set_charges))
+energies = interaction_energy(set_charges)
 sig_plus = 4/n
 sig_minus = 1.0
 #caluclates array of values with given beta_resolution between (-\sigma^+, \sigma^-)
@@ -27,46 +26,26 @@ beta_vals, beta_step = np.linspace(-sig_plus, sig_minus, beta_resolution + 1, re
 beta_vals = beta_vals[1:-1]
 
 # print(len(beta_vals))
-# beta_vals = beta_vals[-(beta_resolution//10):]
+# beta_vals = [val for ]
 
 
 prob_figs = []
 Zfig, Zax = plt.subplots()
 Zax.set_xlabel(r'$\beta$')
 Zax.set_ylabel(r'$Z_I(\beta)$')
-Zax.set_title(f'Partition Function $Z_I(\\beta)$ vs $\\beta$\n(N={n}, q={set_charges}, step={beta_step:.3f})')
+Zax.set_title(f'Partition Function $Z_I(\\beta)$\n(N={n}, q={set_charges}, step={beta_step:.4f})')
+Zfig.canvas.manager.set_window_title(f"Z_I(β)")
 
 Efig, Eax = plt.subplots()
 Eax.set_xlabel(r'$\beta$')
 Eax.set_ylabel(r'${\langle E \rangle}_\beta$')
-Eax.set_title(f'Expected value vs $\\beta$\n(N={n}, q={set_charges}, step={beta_step:.3f})')
+Eax.set_title(f'Expected value vs $\\beta$\n(N={n}, q={set_charges}, step={beta_step:.4f})')
+Efig.canvas.manager.set_window_title("<E>_β")
+Eax.set_yscale("symlog")
 
 
 for p in primes:
-    # create connection
-    dbname = f"./size{n}.db"
-    # print(dbname)
-    con = sqlite3.connect(dbname)
-    
-    # Read query results into a pandas DataFrame
-    query = f"""
-        SELECT
-            trees.id,
-            trees.branches,
-            trees.degrees,
-            probabilities.prime,
-            probabilities.probability
-        FROM
-            probabilities
-        JOIN
-            trees ON probabilities.tree_id = trees.id
-        WHERE
-            probabilities.prime = {p}
-        ORDER BY
-            probabilities.probability DESC
-    """
-    df = pd.read_sql_query(query, con)
-    con.close()
+    df = query(n, p)
 
     # do some formatting as arrays
     df["branches"] = df["branches"].apply(ast.literal_eval)
@@ -77,7 +56,7 @@ for p in primes:
     expected = []
     df_arr: List[pd.DataFrame] = []
     
-    # Computes Z_I(\beta) and P_tree(p, \beta) for all betas
+    # Computes Z_I(\beta), Expected value, and P_tree(p, \beta) for all betas
     for i, beta in enumerate(beta_vals):
 
         df_beta = pd.DataFrame({
@@ -91,40 +70,42 @@ for p in primes:
         #calculates sum and Z_I(\beta)
         total = sum(df_beta["terms"])
         Z_i_beta = (p ** (energies[-1]*beta)) * total
-        
+        Z_i.append(Z_i_beta)
+
         #computes probability for all trees
         df_beta['phys_prob'] = df_beta['terms']/total
         
-        weights = df.apply(lambda row : weight(row["branches"], row["degrees"], p, energies, beta), axis=1)
-        print(weights.head())
+        #computes expected value
+        weights = df.apply(lambda row : weight(row["branches"], p, energies, beta), axis=1)
         expected_beta = math.log(p) * (-energies[-1] + sum(df_beta["phys_prob"]*weights))
-        
-        df_arr.append(df_beta)
-        Z_i.append(Z_i_beta)
         expected.append(expected_beta)
+
+        df_arr.append(df_beta)
         
         # print(f"beta: {beta}, Z_I = {Z_i_beta}\n", df_beta.head(), "\n--------\n")
     
-    # start = 800
-    # end = 900
-    start = 0
-    end = len(df_arr[0])
-
-
-    all_phys_probs = [frame["phys_prob"].values for frame in df_arr]
     # Set up probability plot with slider
     fig, ax = plt.subplots(figsize=(7, 4.5))
     plt.subplots_adjust(bottom=0.4)
-    # Create x-axis labels
-    xlabels = [f"{tid} ({idx})" for tid, idx in zip(df['id'][start:end], df.index[start:end])]
-    bar_container = ax.bar(xlabels, all_phys_probs[0][start:end])
     ax.set_xlabel('Tree ID (row index)')
     ax.set_ylabel('Probability')
-    ax.set_title(f'Physical Probability of Trees\n(N={n}, q={set_charges}, p={p}, step={beta_step:.3f})')
+    ax.set_title(f'Physical Probability of Trees\n(N={n}, q={set_charges}, p={p}, step={beta_step:.4f})')
     fig.canvas.manager.set_window_title(f"P={p}")
-    ax.set_ybound(upper=1)
+    # ax.set_ybound(upper=1.0)
+    ax.set_ylim(top=0.5)
     # ax.set_yscale("log")
     plt.xticks(rotation=90)
+    
+    start = 0
+    # end = len(df_arr[0])
+    end = 900
+    
+    # trees = [81, 83, 847, 849]
+
+    # Create x-axis labels and probabilities array
+    all_phys_probs = [frame["phys_prob"].values for frame in df_arr]
+    xlabels = [f"{tid} ({idx})" for tid, idx in zip(df['id'][start:end], df.index[start:end])]
+    bar_container = ax.bar(xlabels, all_phys_probs[0][start:end])
 
     # beta slider
     ax_slider = plt.axes([0.15, 0.02, 0.7, 0.02])# left,bottom,width,height
@@ -165,6 +146,10 @@ for p in primes:
         print(f"Saving animation to {filename}...")
         anim.save(filename, writer='ffmpeg')
         print(f"Animation saved to {filename}")
+        
+    save = "n"
+    if (save.lower() == "y"):
+        save_beta_animation(fig, ax, bar_container, all_phys_probs, beta_vals, filename=f'animations/prob_p{p}_n{n}_b{beta_step}.mp4')
 
     plt.tight_layout(pad=3.0)
     prob_figs.append(fig)
@@ -175,6 +160,9 @@ for p in primes:
 Zax.legend(title="Prime p")
 Eax.legend(title="Prime p")
 Zax.set_yscale("log")
+
+
+        
 # for fig in figs:
 #         # Save animation after showing interactive plot
 #     # save = input("Do you want to save this as a .mp4? (y/n) ")
